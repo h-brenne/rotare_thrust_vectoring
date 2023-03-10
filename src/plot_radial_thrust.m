c3e11e44bccfb22da85b89004b7d3037eca348d7
Håvard Brenne <haavbren@ntnu.no>
2023-03-10 10:03:47 +0100

    feat: Radial plot implementation

---
+++ src/plot_radial_thrust.m
@@ -0,0 +1,61 @@
+clear;
+load('../results/havard_stahlhut_tip.mat');
+dT = OpRot(1,1).ElPerf.dT;
+dQ = OpRot(1,1).ElPerf.dT;
+figure(1)
+plot(dQ);
+
+% Root cutout is not included in simulation, need to 
+% prepend for vizualization
+num_cutout_elems = OpRot(1,1).Rot.cutout/OpRot(1,1).Rot.Bl.dy;
+dT = [zeros(1, round(num_cutout_elems)) dT];
+dQ = [zeros(1, round(num_cutout_elems)) dQ];
+%plot(dT);
+
+% 3D plot of Thrust
+%cylinder(dT)
+n = 20;
+theta = linspace(0,pi,n).';
+
+x0 = linspace(1,length(dT), length(dT));
+y0 = dT(x0);
+y = x0.*cos(theta);
+x = x0.*sin(theta);
+z = repmat(y0,[n,1]);
+
+figure(2)
+h = surf(x,y,z);
+hold on
+set(h, 'EdgeAlpha',0.0);
+set(h, 'FaceAlpha',1);
+theta = linspace(pi,2*pi,n).';
+
+x0 = linspace(1,length(dT), length(dT));
+y0 = dT(x0);
+y = x0.*cos(theta);
+x = x0.*sin(theta);
+z = repmat(y0,[n,1]);
+h = surf(x,y,z);
+hold on
+set(h, 'EdgeAlpha',0.0);
+set(h, 'FaceAlpha',0.2);
+axHand = h.Parent;
+axHand.ZLim = [-0.2, 0.2];
+axHand.XLim = [-length(dT), length(dT)];
+axHand.YLim = [-length(dT), length(dT)];
+% Draw 2d plot
+y = linspace(-length(dT),length(dT), 2*length(dT));
+x = zeros(1,2*length(dT));
+z = zeros(1,2*length(dT));
+z(1:length(dT)) = flip(dT);
+z(1+length(dT):2*length(dT)) = dT;
+
+plot3(x,y,z,'linewidth',2, 'color','red');
+
+set(gca,'XTick',[]);
+set(gca,'YTick',[]);
+set(gca,'ZTick',[]);
+zlabel('dT');
+title('Radial thrust distrubution')
+%set(h,'zscale','log')
+view(-50,50)
